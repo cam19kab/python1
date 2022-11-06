add76a2e6f00361ebd8dba88de5907f5be950b90
cam19kab@gmail.com <cam19kab@gmail.com>
2022-11-06 20:30:31 +0100

    Complete modifcation image

--- Web.py
+++ Web.py
@@ -1,9 +1,19 @@
-from flask import Flask, render_template,Flask
-#import cv2
-#import mediapipe as mp
+from flask import Flask, render_template,Flask,Response
+import cv2
+import mediapipe as mp
 
-#Realizar nuestra VideoCaptura
+# Drawing Funtion
+# Create own drawing function
 
+mpDibujo = mp.solutions.drawing_utils
+ConfDibu =mpDibujo.DrawingSpec(thickness=1,circle_radius=1)
+
+# Create a object where we are save all the face
+
+mpMallaFacial = mp.solutions.face_mesh
+MallaFacial = mpMallaFacial.FaceMesh(max_num_faces=1)
+
+# VideoCapture
 cap = cv2.VideoCapture(0)
 
 def gen_frame():
@@ -15,6 +25,15 @@ def gen_frame():
         else:
             suc, encode = cv2.imencode('.jpg',frame)
             frame = encode.tobytes()
+            frameRGB = cv2.cvtColor(frame, cv2.COLOR_BGRA2RGB)
+            resultados = MallaFacial.process(frameRGB)
+
+            # If we detect some face
+            if resultados.multi_face_landmarks:
+                # iteration
+                for rostros in resultados.multi_face_landmarks:
+                    #drawing
+                    mpDibujo.draw_landmarks(frame, rostros, mpMallaFacial.FACEMESH_TESSELATION, ConfDibu, ConfDibu)
 
         yield(b'--frame\r\n'
             b'content-Type: image/jpeg\r\n\r\n'+ frame + b'\r\n')
